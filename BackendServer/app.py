a1e1f4c01f98a2f28105b70487ed3f5485fb04a1
MedaAkhil <medaakhilaeshchowdary@gmail.com>
2025-07-07 01:16:49 +0530

    localhost ip changed

--- BackendServer/app.py
+++ BackendServer/app.py
@@ -36,4 +36,4 @@ def nseCompanies():
 if __name__ == "__main__":
     import uvicorn
     port = int(os.environ.get("PORT", 10000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
+    uvicorn.run("main:app", host="127.0.0.1", port=port)
