28718c53bb8295efbfc32160265150b2d992b6aa
MedaAkhil <medaakhilaeshchowdary@gmail.com>
2025-07-07 01:12:55 +0530

    app run error resolved

--- BackendServer/app.py
+++ BackendServer/app.py
@@ -34,5 +34,6 @@ def nseCompanies():
     return getListOfNseCompanies()
     
 if __name__ == "__main__":
+    import uvicorn
     port = int(os.environ.get("PORT", 10000))
-    app.run(app, host="0.0.0.0", port=port)
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
