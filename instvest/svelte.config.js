553a472319738c57d02592bcfd4ea2e57669e81d
Akhil Chowdary <94841034+MedaAkhil@users.noreply.github.com>
2025-07-07 13:36:04 +0530

    jvud

--- instvest/svelte.config.js
+++ instvest/svelte.config.js
@@ -6,9 +6,7 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter({
-		      out: 'build' // optional, default is 'build'
-	})}
+	kit: { adapter:adapter ()}
 };
 
 export default config;
